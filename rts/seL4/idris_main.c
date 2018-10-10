3d95412c43fed5a1631e728173b5abbf7221a9d4
Jonas Claeson <jonas.cl@protonmail.com>
2018-10-10 18:32:29 +0200

    Shrunk the heap and stack sizes
    The sizes had to be shrunk in order for the Idris "hello world" app
    to run in QEMU without updating settings.

--- rts/seL4/idris_main.c
+++ rts/seL4/idris_main.c
@@ -6,8 +6,8 @@
 void _idris__123_runMain_95_0_125_(VM* vm, VAL* oldbase);
 
 RTSOpts opts = {
-    .init_heap_size = 16384000,
-    .max_stack_size = 4096000,
+    .init_heap_size = 100000,
+    .max_stack_size = 5000,
     .show_summary   = 0
 };
 
