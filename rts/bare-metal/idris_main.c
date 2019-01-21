9d881d7c1c97c57762fcd75f476c9a2b37f97cfc
Jonas Claeson <jonas.cl@protonmail.com>
2019-01-21 10:39:21 +0100

    Add UART logging to the bare-metal app for debugging

--- rts/bare-metal/idris_main.c
+++ rts/bare-metal/idris_main.c
@@ -3,6 +3,8 @@
 #include "idris_rts.h"
 #include "idris_stats.h"
 
+#include "consoleUtils.h"
+
 void _idris__123_runMain_95_0_125_(VM* vm, VAL* oldbase);
 
 RTSOpts opts = {
@@ -12,6 +14,14 @@ RTSOpts opts = {
 };
 
 int main() {
+    /* Initialize the UART console */
+    ConsoleUtilsInit();
+
+    /* Select the console type based on compile time check */
+    ConsoleUtilsSetType(CONSOLE_UART);
+
+    ConsoleUtilsPrintf("Setup UART logging\n");
+
     VM* vm = init_vm(opts.max_stack_size, opts.init_heap_size, 1);
     init_gmpalloc();
     init_nullaries();
